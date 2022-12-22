e8afd0147f563613dc708f3a4854f19191113daf
Chamaeleo Calyptratus <39703558+henninggaus@users.noreply.github.com>
2022-12-22 04:17:03 +0100

    ping

---
+++ post-quantum-crypto/example/post_quantum_crypto_example.dart
@@ -0,0 +1,6 @@
+import 'package:post_quantum_crypto/post_quantum_crypto.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ post-quantum-crypto/lib/post_quantum_crypto.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library post_quantum_crypto;
+
+export 'src/post_quantum_crypto_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
